1296fdb085d1de9a38d73c701b0c71ac86eb67a6
jens.struwe <jens.struwe@4177d21b-af57-ffb9-09be-8f22e8e4d1cc>
2011-06-21 07:30:07 +0000

    [Blog] Download error modifications

---
+++ osflash/themes/sibirjak/includes/sibirjak_show_download_404.inc.php
@@ -0,0 +1,11 @@
+<!--BEGIN article -->
+<div id="post-0" class="<?php echo join(' ', get_post_class()); ?>">
+
+	<div class="entry-content article">
+		<h1 class="post-title">File Not Found</h1>
+		
+		<p>The file you are trying to download does not exist.</p>
+	</div>
+
+</div>
+<!--END article -->
